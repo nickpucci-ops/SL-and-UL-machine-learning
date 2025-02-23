4c0135ec8149df779b0e3feab872d4301f0e5078
Nick Pucci <nickpucci88@gmail.com>
2025-02-23 14:13:07 -0600

    Fix header=None to display points

--- script.py
+++ script.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import csv
 
-x = pd.read_csv('x.csv')
+x = pd.read_csv('x.csv', header=None)
 y = pd.read_csv('y.csv')
-z = pd.read_csv('z.csv')
+z = pd.read_csv('z.csv', header=None)
 
 #2D scatter plots
-plt.scatter(x, z, alpha = 0.5)
+plt.scatter(x, z, label='x vs z', alpha = 0.5)
 plt.show()
