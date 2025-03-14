4759ad1bb5d8019efea861c6df9a28edc7568dbe
Nick Pucci <nickpucci88@gmail.com>
2025-03-13 21:36:39 -0500

    Add testing splits, 80% training, 20%testing

--- supervised-learning/train_model.py
+++ supervised-learning/train_model.py
@@ -13,29 +13,38 @@ z = pd.read_csv('data/z.csv', header=None).values.flatten()
 
 X = np.column_stack((x,y)) #combine x and y into the feature matrix 'X' (50, 2)
 
+#training splits: 80% training, 20% test
+X_train, X_test, z_train, z_test = train_test_split(X, z, test_size=0.2, random_state=42)
+
 #scale the features and the target due to the large z values
 scaler_X = StandardScaler()
-X_scaled = scaler_X.fit_transform(X)  # Scale x, y
+X_train_scaled = scaler_X.fit_transform(X_train)
+X_test_scaled = scaler_X.transform(X_test)
+
 scaler_z = StandardScaler(with_mean=True, with_std=True)
-z_scaled = scaler_z.fit_transform(z.reshape(-1, 1)).ravel()
+z_train_scaled = scaler_z.fit_transform(z_train.reshape(-1, 1)).ravel()
+z_test_scaled = scaler_z.transform(z_test.reshape(-1, 1)).ravel()
 
 #Implement polynomial features
 poly = PolynomialFeatures(degree=3)  # (1, x, y, x^2, xy, y^2, x * y^2, x^2 * y, y^3, x^3 )
-X_poly = poly.fit_transform(X_scaled)  #(50, 10)
+X_train_poly = poly.fit_transform(X_train_scaled)  #(50, 10)
+X_test_poly = poly.transform(X_test_scaled)
 
 alpha_constant = 100.0
 #traing model with ridge regression, saving trained model, scalars, and polynomial transformer
 model_ridge = Ridge(alpha=alpha_constant)#alpha is a constant that multiplies the L2 term (the penalty) and controls the regularization strength
-model_ridge.fit(X_poly, z_scaled)
+model_ridge.fit(X_train_poly, z_train_scaled)
 
 model_no_ridge = LinearRegression()#alpha=0 is equivalent to linear regression
-model_no_ridge.fit(X_poly, z_scaled)
+model_no_ridge.fit(X_train_poly, z_train_scaled)
 
 trained_data_ridge = {
     'model': model_ridge,
     'scaler_X': scaler_X,
     'scaler_z': scaler_z,
-    'poly': poly
+    'poly': poly,
+    'X_test': X_test, #test data saved for visualization
+    'z_test': z_test
 }
 with open('trained_models/trained_model_ridge.pkl', 'wb') as f:
     pickle.dump(trained_data_ridge, f)
@@ -45,7 +54,9 @@ trained_data_no_ridge = {
     'model': model_no_ridge,
     'scaler_X': scaler_X,
     'scaler_z': scaler_z,
-    'poly': poly
+    'poly': poly,
+    'X_test': X_test,
+    'z_test': z_test
 }
 with open('trained_models/trained_model_no_ridge.pkl', 'wb') as f:
     pickle.dump(trained_data_no_ridge, f)
